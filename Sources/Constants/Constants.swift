22f316c23365ff6e441e3439c87a0b569b08b60e
Handy <handy.kwok@gmail.com>
2024-04-20 16:08:25 +0700

    add constants

--- Sources/Constants/Constants.swift
+++ Sources/Constants/Constants.swift
@@ -1,2 +1,31 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+public struct Constants {
+    public static let empty = ""
+    
+    public static let zero = 0.0
+    public static let _1 = 1.0
+    public static let _2 = 2.0
+    public static let _4 = 4.0
+    public static let _8 = 8.0
+    public static let _12 = 12.0
+    public static let _16 = 16.0
+    public static let _20 = 20.0
+    public static let _24 = 24.0
+    public static let _28 = 28.0
+    public static let _32 = 32.0
+    public static let _36 = 36.0
+    public static let _40 = 40.0
+    public static let _44 = 44.0
+    public static let _48 = 48.0
+    public static let _50 = 50.0
+    public static let _60 = 60.0
+    public static let _70 = 70.0
+    public static let _80 = 80.0
+    public static let _90 = 90.0
+    public static let _100 = 100.0
+    
+}
